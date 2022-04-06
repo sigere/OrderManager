c3bab2da62a78489f3101125b6f42cace46985ba
Krzysztof Marczak <a.marczak1324@gmail.com>
2022-04-06 20:11:06 +0200

    fix config

--- migrations/Version20220404175313.php
+++ migrations/Version20220404175313.php
@@ -14,7 +14,7 @@ final class Version20220404175313 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Repertory Entry entity, additional_fee, refactor states to en, clear preferences';
     }
 
     public function up(Schema $schema): void
