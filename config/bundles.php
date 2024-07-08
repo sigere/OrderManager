c047efffe32e78c24fc4ead596c8d66c7b715d7c
Krzysztof Marczak <a.marczak1324@gmail.com>
2024-07-08 19:21:34 +0200

    Encore & Stimulus

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
