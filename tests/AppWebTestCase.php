61597625fe9c1b39052d19e4b206cef656fee98e
Krzysztof Marczak <a.marczak1324@gmail.com>
2024-08-11 21:50:22 +0200

    github flows

--- tests/AppWebTestCase.php
+++ tests/AppWebTestCase.php
@@ -38,6 +38,9 @@ abstract class AppWebTestCase extends WebTestCase
 
     protected function getAppClient(string $alias): Client
     {
-        return $this->entityManager->getRepository(Client::class)->findOneBy(['alias' => $alias]);
+        var_dump($this->entityManager->getRepository(Client::class)->findAll());
+        return $this->entityManager
+            ->getRepository(Client::class)
+            ->findOneBy(['alias' => $alias]);
     }
 }
